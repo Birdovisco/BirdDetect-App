abed2342656203c026c93a86d76137d49b022dd3
@patryk.bielecki <bielecki.biz@gmail.com>
2024-04-21 18:55:12 +0200

    BIRD-30 screens:BirdCatalog.tsx: Added bird catalog screen

---
+++ screens/BirdCatalog.tsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect, useMemo} from "react";
+import { View, Text } from "react-native";
+
+export default function Detect({ navigation }) {
+
+    return (
+        <View>
+            <Text>Bird catalog screen</Text>
+        </View>
+    );
+}
