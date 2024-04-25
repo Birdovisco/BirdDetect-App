9c57021cb3019ed8d8be36259517a8d7f9ea3abb
Patryk Bielecki <bielecki.biz@gmail.com>
2024-04-26 01:23:04 +0200

    BIRD-30 screens: Detect.tsx: initial button style

--- screens/Detect.tsx
+++ screens/Detect.tsx
@@ -1,11 +1,26 @@
-import React, {useState, useEffect, useMemo} from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, TouchableOpacity, Image, Text} from 'react-native';
+import { theme } from '../core/theme';
 
-export default function Detect({ navigation }) {
+const ScreenWithButton = () => {
+
+    const handleButtonPress = () => {
+        console.log('Button pressed!');
+    };
 
     return (
-        <View >
-            <Text>Detect screen</Text>
+        <View
+            className="flex-1 justify-center items-center bg-theme-primary"
+            style={{backgroundColor: theme.colors.primary}}>
+            <Text className="text-white text-lg mb-4">Naciśnij aby nagrać dźwięk</Text>
+            <TouchableOpacity
+                onPress={handleButtonPress}
+                className="w-56 h-56 rounded-full bg-white justify-center items-center"
+            >
+                <Image source={require('../assets/icon.png')} className="w-48 h-48" />
+            </TouchableOpacity>
         </View>
     );
 }
+
+export default ScreenWithButton;
