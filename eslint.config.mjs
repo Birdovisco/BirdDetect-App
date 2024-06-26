23788ed1fad7c0fabdd79556a52135e9be5f47b9
Michał Kuprianowicz <michal.kuprianowicz@interia.pl>
2024-06-26 19:36:38 +0200

    Revert "eslint config change"
    This reverts commit 7a1cbb84e0c83db0b9f312978a8e5bbc08da7412.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,29 +5,9 @@ import { fixupConfigRules } from "@eslint/compat";
 
 
 export default [
-  {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-    languageOptions: {
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-    },
-    plugins: {
-      '@typescript-eslint': tsEslint,
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react/recommended',
-    ],
-    rules: {
-      // Add or customize rules here
-    },
-  },
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
 ];
