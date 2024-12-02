5be0f4fce5b767d011d2d3f38628c9d51c8a907c
Patryk Bielecki <bielecki.biz@gmail.com>
2024-12-02 12:02:20 +0100

    BIRD-57 linting configured for react native
    Signed-off-by: Patryk Bielecki <bielecki.biz@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,36 @@
 import globals from "globals";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  ...tseslint.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      react: pluginReactConfig,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...pluginReactConfig.rules,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ];
