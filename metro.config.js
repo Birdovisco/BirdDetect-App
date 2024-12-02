6b61abdc0208fdb5188cb6ae6343b58a04e7e5c6
Patryk Bielecki <bielecki.biz@gmail.com>
2024-12-02 11:14:07 +0100

    BIRD-57 removed unused files, metro.config update
    Signed-off-by: Patryk Bielecki <bielecki.biz@gmail.com>

--- metro.config.js
+++ metro.config.js
@@ -1,24 +1,7 @@
-const path = require("path");
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const { getDefaultConfig } = require("expo/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
-const { resolver: { sourceExts, assetExts } } = defaultConfig;
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {
-  transformer: {
-    babelTransformerPath: require.resolve("react-native-svg-transformer"),
-  },
-  resolver: {
-    assetExts: [...assetExts, 'bin', 'json'],
-    sourceExts: [...sourceExts, "svg", 'json'],
-  },
-  watchFolders: [path.resolve(__dirname, "../")],
-};
+defaultConfig.resolver.assetExts.push("bin");
 
-module.exports = mergeConfig(defaultConfig, config);
+module.exports = defaultConfig;
